46d25c5063985f1c5776988d305d6a367fb2c9b7
Kledson Basso <kledsonhugo@gmail.com>
2022-05-02 19:32:01 -0300

    Create db-example.tf

---
+++ checkpoint-02/terraform/db-example.tf
@@ -0,0 +1,49 @@
+# DB SUBNET GROUP
+resource "aws_db_subnet_group" "rds_vpc10_sn_group" {
+    name       = "rds-vpc10-sn-group"
+    subnet_ids = [ aws_subnet.sn_vpc10_priv_1a.id, aws_subnet.sn_vpc10_priv_1c.id ]
+
+    tags = {
+        Name = "rds-vpc10-sn-group"
+    }
+}
+
+# DB PARAMETER GROUP
+resource "aws_db_parameter_group" "rds_vpc10_pg" {
+    name   = "rds-vpc10-pg"
+    family = "mysql8.0"
+    
+    parameter {
+        name  = "character_set_server"
+        value = "utf8"
+    }
+    
+    parameter {
+        name  = "character_set_database"
+        value = "utf8"
+    }
+}
+
+# DB INSTANCE
+resource "aws_db_instance" "rds_db_notifier" {
+    identifier             = "rds-db-notifier"
+    engine                 = "mysql"
+    engine_version         = "8.0.23"
+    instance_class         = "db.t2.micro"
+    storage_type           = "gp2"
+    allocated_storage      = "20"
+    max_allocated_storage  = 0
+    monitoring_interval    = 0
+    name                   = "notifier"
+    username               = "admin"
+    password               = "adminpwd"
+    skip_final_snapshot    = true
+    db_subnet_group_name   = aws_db_subnet_group.rds_vpc10_sn_group.name
+    parameter_group_name   = aws_db_parameter_group.rds_vpc10_pg.name
+    vpc_security_group_ids = [ aws_vpc.vpc10.id ]
+
+    tags = {
+        Name = "rds-db-notifier"
+    }
+
+}
